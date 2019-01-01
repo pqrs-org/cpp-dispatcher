ffaa5c8723741bef6d761ef8a6da68ce6b329e60
Takayama Fumihiko <tekezo@pqrs.org>
2019-01-01 17:17:23 +0900

    update tests

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -4,24 +4,28 @@
 #include <pqrs/dispatcher.hpp>
 
 TEST_CASE("object_id") {
+  auto active_object_id_count = pqrs::dispatcher::active_object_id_count();
+  int id1 = 0;
+
   {
     pqrs::dispatcher::object_id object_id1(pqrs::dispatcher::make_new_object_id());
     pqrs::dispatcher::object_id object_id2(pqrs::dispatcher::make_new_object_id());
 
-    REQUIRE(object_id1.get() == 1);
-    REQUIRE(object_id2.get() == 2);
-    REQUIRE(pqrs::dispatcher::active_object_id_count() == 2);
+    id1 = object_id1.get();
+
+    REQUIRE(object_id2.get() == id1 + 1);
+    REQUIRE(pqrs::dispatcher::active_object_id_count() == active_object_id_count + 2);
   }
 
-  REQUIRE(pqrs::dispatcher::active_object_id_count() == 0);
+  REQUIRE(pqrs::dispatcher::active_object_id_count() == active_object_id_count);
 
   {
     pqrs::dispatcher::object_id object_id3(pqrs::dispatcher::make_new_object_id());
     pqrs::dispatcher::object_id object_id4(pqrs::dispatcher::make_new_object_id());
 
-    REQUIRE(object_id3.get() == 3);
-    REQUIRE(object_id4.get() == 4);
-    REQUIRE(pqrs::dispatcher::active_object_id_count() == 2);
+    REQUIRE(object_id3.get() == id1 + 2);
+    REQUIRE(object_id4.get() == id1 + 3);
+    REQUIRE(pqrs::dispatcher::active_object_id_count() == active_object_id_count + 2);
   }
 }
 
@@ -611,58 +615,6 @@ TEST_CASE("dispatcher.when") {
       REQUIRE(string == "abdefcg");
 
       d.terminate();
-    }
-  }
-}
-
-namespace {
-class when_hardware_time_source_test final : public pqrs::dispatcher::extra::dispatcher_client {
-public:
-  when_hardware_time_source_test(std::weak_ptr<pqrs::dispatcher::dispatcher> weak_dispatcher,
-                                 size_t& count,
-                                 pqrs::dispatcher::duration duration) : dispatcher_client(weak_dispatcher) {
-    enqueue_to_dispatcher(
-        [&count] {
-          std::cout << "." << std::flush;
-          ++count;
-        },
-        when_now() + duration);
-  }
-
-  virtual ~when_hardware_time_source_test(void) {
-    detach_from_dispatcher([] {
-    });
-  }
-};
-} // namespace
-
-TEST_CASE("dispatcher.when_hardware_time_source") {
-  std::cout << "dispatcher.when_hardware_time_source" << std::endl;
-
-  auto time_source = std::make_shared<pqrs::dispatcher::hardware_time_source>();
-
-  {
-    size_t count = 0;
-
-    {
-      auto d = std::make_shared<pqrs::dispatcher::dispatcher>(time_source);
-
-      std::vector<std::unique_ptr<when_hardware_time_source_test>> objects;
-      for (int i = 0; i < 10; ++i) {
-        objects.push_back(std::make_unique<when_hardware_time_source_test>(
-            d,
-            count,
-            std::chrono::milliseconds(i * 100)));
-      }
-
-      std::this_thread::sleep_for(std::chrono::milliseconds(500));
-
-      REQUIRE(count > 2);
-      REQUIRE(count < 8);
-
-      d->terminate();
-
-      std::cout << std::endl;
     }
   }
 }
