aa444548eefc4d21ac46eea0d7cd832bf7c7e48f
Takayama Fumihiko <tekezo@pqrs.org>
2019-01-01 19:28:36 +0900

    fix a deadlock in detached function

--- include/pqrs/dispatcher/dispatcher.hpp
+++ include/pqrs/dispatcher/dispatcher.hpp
@@ -11,6 +11,7 @@
 #include <deque>
 #include <exception>
 #include <mutex>
+#include <optional>
 #include <pqrs/thread_wait.hpp>
 #include <thread>
 
@@ -110,9 +111,29 @@ public:
         }
 
         if (e) {
-          std::lock_guard<std::mutex> lock(function_mutex_);
+          // Set running_function_object_id_
+
+          {
+            std::lock_guard<std::mutex> lock(running_function_object_id_mutex_);
+
+            running_function_object_id_ = e->get_object_id_value();
+          }
+
+          running_function_object_id_cv_.notify_all();
+
+          // Run function
 
           e->call_function();
+
+          // Unset running_function_object_id_
+
+          {
+            std::lock_guard<std::mutex> lock(running_function_object_id_mutex_);
+
+            running_function_object_id_ = std::nullopt;
+          }
+
+          running_function_object_id_cv_.notify_all();
         }
       }
     });
@@ -174,9 +195,14 @@ public:
                    std::end(queue_));
     }
 
-    if (!dispatcher_thread()) {
+    if (!dispatcher_thread() && !running_detached_function()) {
       // Wait until current running function is finised.
-      std::lock_guard<std::mutex> lock(function_mutex_);
+
+      std::unique_lock<std::mutex> lock(running_function_object_id_mutex_);
+
+      running_function_object_id_cv_.wait(lock, [this, &object_id] {
+        return running_function_object_id_ != object_id.get();
+      });
     }
 
     return true;
@@ -200,7 +226,12 @@ public:
 
     // Execute function
 
-    if (dispatcher_thread()) {
+    if (dispatcher_thread() || running_detached_function()) {
+      // Call function directly in the following cases.
+      // - `detach` is called in the dispatcher thread.
+      // - `detach` is called from another detached function.
+      //   (We have to call detached function directly in order to avoid a deadlock.)
+
       function();
     } else {
       auto w = make_thread_wait();
@@ -227,6 +258,12 @@ public:
 
   bool dispatcher_thread(void) const {
     return std::this_thread::get_id() == worker_thread_id_;
+  }
+
+  bool running_detached_function(void) const {
+    std::lock_guard<std::mutex> lock(running_function_object_id_mutex_);
+
+    return running_function_object_id_ == object_id_.get();
   }
 
   void terminate(void) {
@@ -382,7 +419,9 @@ private:
   std::unordered_set<uint64_t> object_ids_;
   std::mutex object_ids_mutex_;
 
-  std::mutex function_mutex_;
+  std::optional<uint64_t> running_function_object_id_;
+  mutable std::mutex running_function_object_id_mutex_;
+  std::condition_variable running_function_object_id_cv_;
 };
 } // namespace dispatcher
 } // namespace pqrs
