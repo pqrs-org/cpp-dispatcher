47a4199fd77098b0f4d1652abc696c39a9d175f9
Takayama Fumihiko <tekezo@pqrs.org>
2019-01-03 16:29:22 +0900

    add running_function_object_id_

--- include/pqrs/dispatcher/dispatcher.hpp
+++ include/pqrs/dispatcher/dispatcher.hpp
@@ -11,6 +11,7 @@
 #include <deque>
 #include <exception>
 #include <mutex>
+#include <optional>
 #include <pqrs/thread_wait.hpp>
 #include <thread>
 
@@ -110,9 +111,29 @@ public:
         }
 
         if (e) {
-          std::lock_guard<std::mutex> lock(function_mutex_);
+          // Set running_function_object_id_
+
+          {
+            std::lock_guard<std::mutex> lock(running_function_object_id_mutex_);
+
+            running_function_object_id_ = e->get_object_id_value();
+          }
+
+          running_function_object_id_cv_.notify_all();
+
+          // Run function
 
           e->call_function();
+
+          // Unset running_function_object_id_
+
+          {
+            std::lock_guard<std::mutex> lock(running_function_object_id_mutex_);
+
+            running_function_object_id_ = std::nullopt;
+          }
+
+          running_function_object_id_cv_.notify_all();
         }
       }
     });
@@ -175,8 +196,13 @@ public:
     }
 
     if (!dispatcher_thread()) {
-      // Wait until current running function is finised.
-      std::lock_guard<std::mutex> lock(function_mutex_);
+      // Wait the running function if the running function is owned by object_id.
+
+      std::unique_lock<std::mutex> lock(running_function_object_id_mutex_);
+
+      running_function_object_id_cv_.wait(lock, [this, &object_id] {
+        return running_function_object_id_ != object_id.get();
+      });
     }
 
     return true;
@@ -229,6 +255,12 @@ public:
 
   bool dispatcher_thread(void) const {
     return std::this_thread::get_id() == worker_thread_id_;
+  }
+
+  bool running_detached_function(void) const {
+    std::lock_guard<std::mutex> lock(running_function_object_id_mutex_);
+
+    return running_function_object_id_ == object_id_.get();
   }
 
   void terminate(void) {
@@ -381,11 +413,14 @@ private:
   std::mutex mutex_;
   std::condition_variable cv_;
 
+  // `object_id_` is for a function after detach
   object_id object_id_;
   std::unordered_set<uint64_t> object_ids_;
   std::mutex object_ids_mutex_;
 
-  std::mutex function_mutex_;
+  std::optional<uint64_t> running_function_object_id_;
+  mutable std::mutex running_function_object_id_mutex_;
+  std::condition_variable running_function_object_id_cv_;
 };
 } // namespace dispatcher
 } // namespace pqrs
