893915f5f3762c8db0f9f789dc980bf05d98dc87
Takayama Fumihiko <tekezo@pqrs.org>
2022-12-26 08:55:34 +0900

    Update tests (add destroy timer itself in callback)

--- tests/src/timer_test.hpp
+++ tests/src/timer_test.hpp
@@ -7,11 +7,14 @@ class timer_test final : public pqrs::dispatcher::extra::dispatcher_client {
 public:
   timer_test(std::weak_ptr<pqrs::dispatcher::dispatcher> weak_dispatcher,
              size_t& count,
-             std::chrono::milliseconds duration) : dispatcher_client(weak_dispatcher),
-                                                   timer_(*this) {
+             std::chrono::milliseconds duration,
+             const std::function<void(void)>& function)
+      : dispatcher_client(weak_dispatcher),
+        timer_(*this) {
     timer_.start(
-        [&count] {
+        [&count, function] {
           ++count;
+          function();
         },
         duration);
   }
@@ -47,7 +50,7 @@ void run_timer_test(void) {
       {
         auto d = std::make_shared<pqrs::dispatcher::dispatcher>(time_source);
 
-        auto t = std::make_unique<timer_test>(d, count, std::chrono::milliseconds(100));
+        auto t = std::make_unique<timer_test>(d, count, std::chrono::milliseconds(100), [] {});
 
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
@@ -59,6 +62,28 @@ void run_timer_test(void) {
 
         d->terminate();
       }
+    }
+  };
+
+  "dispatcher.timer (destroy timer itself in callback)"_test = [] {
+    std::cout << "dispatcher.timer (destroy timer itself in callback)" << std::endl;
+
+    auto time_source = std::make_shared<pqrs::dispatcher::hardware_time_source>();
+
+    {
+      size_t count = 0;
+
+      auto d = std::make_shared<pqrs::dispatcher::dispatcher>(time_source);
+
+      std::shared_ptr<timer_test> t;
+
+      t = std::make_shared<timer_test>(d, count, std::chrono::milliseconds(100), [&t] { t = nullptr; });
+
+      std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+      expect(count == 1_i);
+
+      d->terminate();
     }
   };
 }
