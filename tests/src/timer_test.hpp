055a0b26e25c2f2913e3f24a6999bbc4eed8509f
Takayama Fumihiko <tekezo@pqrs.org>
2022-12-29 10:23:21 +0900

    Update tests

--- tests/src/timer_test.hpp
+++ tests/src/timer_test.hpp
@@ -86,4 +86,27 @@ void run_timer_test(void) {
       d->terminate();
     }
   };
+
+  "dispatcher.timer (stop timer immediately)"_test = [] {
+    std::cout << "dispatcher.timer (stop timer immediately)" << std::endl;
+
+    auto time_source = std::make_shared<pqrs::dispatcher::hardware_time_source>();
+
+    {
+      size_t count = 0;
+
+      auto d = std::make_shared<pqrs::dispatcher::dispatcher>(time_source);
+
+      std::shared_ptr<timer_test> t;
+
+      t = std::make_shared<timer_test>(d, count, std::chrono::milliseconds(100), [] {});
+      t->stop();
+
+      std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+      expect(count == 1_i);
+
+      d->terminate();
+    }
+  };
 }
