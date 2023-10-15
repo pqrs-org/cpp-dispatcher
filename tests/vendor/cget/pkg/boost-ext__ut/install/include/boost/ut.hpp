7b3048d40cd8d4f722bdc6360566161b92bcdbc2
Takayama Fumihiko <tekezo@pqrs.org>
2023-10-15 22:31:40 +0900

    http://www.boost.org/LICENSE_1_0.txt -> https://www.boost.org/LICENSE_1_0.txt

--- tests/vendor/cget/pkg/boost-ext__ut/install/include/boost/ut.hpp
+++ tests/vendor/cget/pkg/boost-ext__ut/install/include/boost/ut.hpp
@@ -3,7 +3,7 @@
 //
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
+// https://www.boost.org/LICENSE_1_0.txt)
 //
 #if defined(__cpp_modules) && !defined(BOOST_UT_DISABLE_MODULE)
 export module boost.ut;
